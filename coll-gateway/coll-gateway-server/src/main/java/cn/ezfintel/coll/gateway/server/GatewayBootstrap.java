bcbd17c843e13a7811ae07dec6c983474dc148c6
wangrong <wr_nba@126.com>
2019-11-29 23:25:23 +0800

    初始化

--- coll-gateway/coll-gateway-server/src/main/java/cn/ezfintel/coll/gateway/server/GatewayBootstrap.java
+++ coll-gateway/coll-gateway-server/src/main/java/cn/ezfintel/coll/gateway/server/GatewayBootstrap.java
@@ -1,10 +1,15 @@
 package cn.ezfintel.coll.gateway.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayBootstrap.class, args);
+    }
 }
